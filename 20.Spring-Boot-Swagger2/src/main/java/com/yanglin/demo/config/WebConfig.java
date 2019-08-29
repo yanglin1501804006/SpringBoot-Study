ceec932efc6f42bd27728f7f41da580c2d1d85e8
yanglin1501804006 <allan1501804006@163.com>
2019-08-29 23:25:50 +0800

    Spring Boot整合Swagger2构建RESTful API

---
+++ 20.Spring-Boot-Swagger2/src/main/java/com/yanglin/demo/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.yanglin.demo.config;
+
+import java.text.SimpleDateFormat;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class WebConfig {
+
+	@Bean
+	public ObjectMapper getObjectMapper() {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+		return mapper;
+	}
+}
