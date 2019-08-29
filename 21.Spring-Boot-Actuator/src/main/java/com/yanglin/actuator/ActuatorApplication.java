2ab5860aa731d05760357dcb1eb23d4085feea5d
yanglin1501804006 <allan1501804006@163.com>
2019-08-29 23:26:18 +0800

    使用Actuator监控Spring Boot应用

---
+++ 21.Spring-Boot-Actuator/src/main/java/com/yanglin/actuator/ActuatorApplication.java
@@ -0,0 +1,21 @@
+package com.yanglin.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ActuatorApplication {
+
+    @RequestMapping("/")
+    String index() {
+        return "hello spring boot ";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+}
